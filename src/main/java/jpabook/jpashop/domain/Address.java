6c558a938d29a2a575c74c24d43bf68bd09f0521
rwh <fkrns1744@naver.com>
2020-08-03 23:01:38 +0900

    [Update: 값 타입 생성자 초기화와 제약]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -6,9 +6,18 @@ import lombok.Setter;
 import javax.persistence.Embeddable;
 
 @Embeddable //  JPA 내장 타입 설정
-@Getter @Setter
+@Getter // 값 타입은 Immutable 해야 함, setter를 아예 제공 안함
 public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() { // 변경 불가능한 제약 걸기, JPA 기본 제약
+    }
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
