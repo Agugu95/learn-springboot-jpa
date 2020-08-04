3180aa1018c624a41e37c31bf4262dc9b9b4023d
rwh <fkrns1744@naver.com>
2020-08-04 23:40:36 +0900

    [Update: 엔티티 연관 관계 편의 메소드 설정]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/domain/Category.java
+++ src/main/java/jpabook/jpashop/domain/Category.java
@@ -8,6 +8,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Getter
 @Setter
@@ -27,11 +29,16 @@ public class Category {
     private List<Item> items = new ArrayList<>();
 
     // 셀프 양방향 연관 관
-    @ManyToOne
+    @ManyToOne (fetch = LAZY)
     @JoinColumn(name = "parent_id")
     private Category parent;
 
     @OneToMany(mappedBy = "parent")
     private List<Category> child = new ArrayList<>();
 
+    // 연관 관계 메소드
+    public void addChildCategory(Category child) { // 셀프 연관 관계
+        this.child.add(child); // 자기 자신을 넣기
+        child.setParent(this); // 부모 설정
+    }
 }
