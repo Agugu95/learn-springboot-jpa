e82e6422c46452fa72c50b08f22828b53625f888
rwh <fkrns1744@naver.com>
2020-08-04 23:40:19 +0900

    [Update: 엔티티 지연 로딩]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -20,4 +20,6 @@ public class Member {
 
     @OneToMany (mappedBy = "member") // 읽기 전용
     private List<Order> orders = new ArrayList<>();
+
+
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.*;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -18,14 +20,14 @@ public class Order {
     @Column(name = "order_id")
     private String id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn (name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery")
     private Delivery delivery;
 
@@ -33,4 +35,19 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // ORDER, CANCEL
+
+    //==연관관계 메서드==//
+    // 연관 관계 편의를 위해 하나의 메소드로 묶어버림
+    public void setMember(Member member) {
+        this.member = member;
+        member.getOrders().add(this);
+    }
+    public void addOrderItem(OrderItem orderItem) {
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+    public void setDelivery(Delivery delivery) {
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
