31f2992ec9f1d5b6a233987ab57d0f92ea5bf905
rwh <fkrns1744@naver.com>
2020-08-12 21:44:49 +0900

    [Update: 아이템 도메인에 따른 Service, Repository(DAO) 구현]
    Signed-off-by: rwh <fkrns1744@naver.com>

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService { // 상품 서비스 클래스는 상품 레포에 위임만 하는 클래스임
+    private final ItemRepository itemRepository;
+
+    @Transactional // 트랜잭셔널 어노테이션은 가까운게 우선권을 가
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
