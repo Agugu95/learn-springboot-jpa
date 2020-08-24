6e50f035047e934512cb4e07e10492024d1debe0
rwh <fkrns1744@naver.com>
2020-08-24 18:20:57 +0900

    [Create: setter 제거 -> 변경 메소드]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/service/ItemService.java
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,12 @@ public class ItemService { // 상품 서비스 클래스는 상품 레포에 위
     @Transactional // 트랜잭셔널 어노테이션은 가까운게 우선권을 가
     public void saveItem(Item item) {
         itemRepository.save(item);
+    }
+
+    @Transactional // 메소드 종료 시 트랜잭셔널 동작, 커밋
+    public void updateItem(Long itemId, String name, int price , int stockQuantity) {
+        Item findItem = itemRepository.findOne(itemId); // 영속성 엔티티로 찾아온 상태
+        findItem.change(name, price, stockQuantity); // setter가 아닌 변경 메소드로 바꾼 상태
     }
 
     public List<Item> findItem() {
