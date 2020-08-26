80ad9750eefad2f28033266610b3ce23c31bcb77
rwh <fkrns1744@naver.com>
2020-08-27 01:13:22 +0900

    [API: 회원 API, DTO 방식]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -1,40 +1,34 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
-@RestController
+@RestController // ResponseBody + Controller
 @RequiredArgsConstructor
 public class MemberApiController {
     private final MemberService memberService;
 
     /**
-     * 등록 V1: 요청 값으로 Member 엔티티를 직접 받는다.
-     * 문제점
-     * - 엔티티에 프레젠테이션 계층을 위한 로직이 추가된다.
-     * - 엔티티에 API 검증을 위한 로직이 들어간다. (@NotEmpty 등등)
-     * - 실무에서는 회원 엔티티를 위한 API가 다양하게 만들어지는데, 한 엔티티에 각각의 API를 위
-     * 한 모든 요청 요구사항을 담기는 어렵다.
-     * - 엔티티가 변경되면 API 스펙이 변한다.
-     * 결론
-     * - API 요청 스펙에 맞추어 별도의 DTO를 파라미터로 받는다.
+     * DTO 미사용 방식
      */
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1() {
+        return memberService.findMembers();
+    }
+
     @PostMapping("/api/v1/members")
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
-    }
-
-    @Data
-    static class CreateMemberRequest {
-        private String name;
     }
 
     @Data
@@ -46,14 +40,68 @@ public class MemberApiController {
         }
     }
 
+    @Data
+    static class CreateMemberRequest { // DTO
+        private String name;
+        private Address address;
+    }
+
     /**
      * 등록 V2: 요청 값으로 Member 엔티티 대신에 별도의 DTO를 받는다.
      */
+
+    @GetMapping("api/v2/members")
+    public Result<List<MemberDto>> memberV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName(), m.getAddress()))
+                .collect(Collectors.toList());
+
+        return new Result<>(collect);
+    }
+
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
         Member member = new Member();
         member.setName(request.getName());
+        member.setAddress(request.getAddress());
+
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}") // 수정
+    public UpdateMemberResponse updateMemberV2(@PathVariable("id") Long id,
+                                               @RequestBody @Valid UpdateMemberRequest request) {
+        memberService.update(id, request.name, request.address); // 로직
+        Member findMember = memberService.findOne(id); // 쿼리
+        return new UpdateMemberResponse(findMember.getId(), findMember.getName(), findMember.getAddress());
+    }
+
+    @Data
+    private static class UpdateMemberRequest { // DTO
+        private String name;
+        private Address address;
+    }
+
+    @Data
+    @AllArgsConstructor
+    private static class UpdateMemberResponse { // DTO
+        private Long id;
+        private String name;
+        private Address address;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+        private Address address;
     }
 }
--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +16,8 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
+    @NotEmpty
+    @NotNull
     private String name;
 
     @Embedded // 내장 타입 임베딩
