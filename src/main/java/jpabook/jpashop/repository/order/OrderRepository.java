879b238bacac22dda5e79988ba11fdf300b53cb2
rwh <fkrns1744@naver.com>
2020-09-02 21:49:27 +0900

    [API: default_batch_fetch_size를 통한 in쿼리 생성]
    Signed-off-by: rwh <fkrns1744@naver.com>

--- src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/OrderRepository.java
@@ -52,13 +52,24 @@ public class OrderRepository { // 래포지토리는 DAO 역할을 수행, 엔�
         return query.getResultList();
     }
 
-    public List<Order> findAllWithMemberDelivery() {
-       return em.createQuery(
+    public List<Order> findAllWithMemberDelivery(int offset, int limit) {
+        return em.createQuery(
                 "select o from Order o" +
                         " join fetch o.member m" +
-                        " join fetch o.delivery d", Order.class
-        ).getResultList();
+                        " join fetch o.delivery d", Order.class)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
     }
+
+    public List<Order> findAllWithMemberDelivery() {
+        return em.createQuery(
+                "select o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d", Order.class)
+                .getResultList();
+    }
+
 
     public List<Order> findAllWithItem() {
         return em.createQuery(" select o from Order o"
